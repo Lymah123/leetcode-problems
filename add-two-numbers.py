c729444d9c3fd6bafd5bb61cb12e857a16261338
oguntunjiharlimatodunola <fimihanodunola625@gmail.com>
2025-04-26 15:49:55 +0100

    Add two numbers problem set

---
+++ add-two-numbers.py
@@ -0,0 +1,49 @@
+# 2. Add Two Numbers
+# Difficulty level: Medium
+# Randomly selected
+# Topics: Linked List, Math, Recursion
+# You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digits. Add the two  numbers and return it as a linked list.
+# You may assume two numbers do not contain any leading zero, except the number 0 itself.
+# Example 1:
+# Input: l1 = [2,4,3], l2 = [5,6,4]
+# Output: [7,0,8]
+# Explanation: 342 + 465 = 807.
+
+# Example 2:
+# Input: l1 = [0], l2 = [0]
+# Output: [0]
+
+# Example 3:
+# Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+# Output: [8,9,9,9,0,0,0,1]
+# Explanation: 9999999 + 9999 = 10009998.
+# Constraints:
+# The number of nodes in each linked list is in the range [1, 100].
+# 0 <= Node.val <= 9
+# It is guaranteed that the list represents a number that does not have leading zeros.
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        dummy_head = ListNode(0)
+        current = dummy_head
+        carry = 0
+
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+
+            total = val1 + val2 + carry
+            carry = total // 10
+            current.next = ListNode(total % 10)
+            current = current.next
+
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+
+        return dummy_head.next
